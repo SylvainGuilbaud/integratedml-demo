744aabe4a08c127a39a451ef199e3388c2525c4d
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2021-01-14 18:18:15 +0100

    sql session

---
+++ integratedML-demo.session.sql
@@ -0,0 +1,277 @@
+
+-- select  diagnosis,count(*) from breastcancer group by Diagnosi
+-- select  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+-- drop model breastcancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelselect  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+-- select  diagnosis,count(*) from breastcancer group by Diagnosi
+-- select  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+-- drop model breastcancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelselect  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelelect count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelselect  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelselect * from BreastCancer
+
+-- select * from passengers
+-- select  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelselect * from BreastCancer
+
+-- select * from passengers
+-- select  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelselect * from BreastCancer
+
+-- select * from passengers
+-- select  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModel
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelelect count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModelselect  diagnosis,count(*) from breastcancer group by Diagnosis
+-- select count(*) from breastcancer
+
+-- create model BreastCancer PREDICTING(diagnosis) from BreastCancer
+
+-- train model BreastCancer from SQLUser.BreastCancer
+
+-- validate model BreastCancer from BreastCancer
+-- SELECT * FROM INFORMATION_SCHEMA.ML_VALIDATION_METRICS
+
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)!=diagnosis
+-- UNION
+-- select predict(breastcancer) as predicted,diagnosis,* 
+-- from breastcancer
+-- where predict(breastcancer)=diagnosis
+
+-- SELECT 
+-- ID, CreatedAt, DefaultTrainedModel, DefaultTrainingQuery, Description, Name, PredictingColumnNames, PredictingColumnTypes, WithColumnNames, WithColumnText, WithColumnTypes
+-- FROM %ML.Model
+
+-- SELECT 
+-- ID, Model, ModelName, ModelType, Provider, TrainedAt, TrainingRun
+-- FROM %ML.TrainedModel
